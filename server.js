242b7b0e9159b39d9c6b997f958aa6c09b8346e3
bilel <bilel.m93@gmail.com>
2021-03-10 22:32:45 +0100

    final

--- server.js
+++ server.js
@@ -3,27 +3,30 @@ const app = express()
 
 const port = 3000
 
+
+
 const TimeMe =(req, res , next)=>{
-console.log('Time :' , Date.now())
+console.log('Time :' , Date.now() , 'Request Type:', req.method  , 'path :' ,req.path)
 next()
 }
 
 
 
-app.get('/' ,TimeMe ,  (req , res)=> 
+
+app.get('/' ,TimeMe , (req , res)=> 
 {
-    console.table({method : req.method , path : req.path })
+    // console.table({method : req.method , path : req.path })
     res.sendFile(__dirname +'/home.html')
 } )
 
 
-app.get('/contact' , (req , res)=> {res.sendFile(__dirname +'/contact.html')}  )
+app.get('/contact' ,TimeMe , (req , res)=> {res.sendFile(__dirname +'/contact.html')}  )
 
 
-app.get('/service' , (req , res)=> {res.sendFile(__dirname +'/service.html')}  )
+app.get('/service' ,TimeMe , (req , res)=> {res.sendFile(__dirname +'/service.html')}  )
 
 
-app.get('/team' , (req , res)=> {res.sendFile(__dirname +'/team.html')}  )
+app.get('/team' ,TimeMe , (req , res)=> {res.sendFile(__dirname +'/team.html')}  )
 
 app.get('/style.css' , (req , res)=> {res.sendFile(__dirname +'/style.css')}  )
 
