279042cecb05f5bb618ec0a48b0d1c70fa6f04ba
bilel <bilel.m93@gmail.com>
2021-03-11 20:51:57 +0100

    fino

--- server.js
+++ server.js
@@ -10,14 +10,25 @@ console.log('Time :' , Date.now() , 'Request Type:', req.method  , 'path :' ,req
 next()
 }
 
+ function testTime (req, res, next) {
+  let date = new Date();
+  let day = date.getDay();
+  let hours = date.getHours();
+  if (day != 6 && day != 0 && hours > 8 && hours < 17) {
+      res.status(200);
+      next();
+  } else {
+      next( res.sendFile(__dirname +'/error.html'));
+  }
+};
 
 
-
-app.get('/' ,TimeMe , (req , res)=> 
+app.get('/' ,TimeMe ,testTime, (req , res)=> 
 {
     // console.table({method : req.method , path : req.path })
     res.sendFile(__dirname +'/home.html')
 } )
+
 
 
 app.get('/contact' ,TimeMe , (req , res)=> {res.sendFile(__dirname +'/contact.html')}  )
